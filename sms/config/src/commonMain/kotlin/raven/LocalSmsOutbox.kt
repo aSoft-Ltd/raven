aacd62464c6d381d5e9a13483c1ea1fb23aabf34
andylamax <andylamax@programmer.net>
2024-09-25 03:37:23 +0300

    Preparing the release of nation, sim and symphony input phone

--- sms/config/src/commonMain/kotlin/raven/LocalSmsOutbox.kt
+++ sms/config/src/commonMain/kotlin/raven/LocalSmsOutbox.kt
@@ -1,5 +1,9 @@
 package raven
 
-internal val isSmsSent: SendSmsParams.(String) -> Boolean = { receiver -> receiver in to }
+internal val isSmsSent: SendSmsParams.(String) -> Boolean = { receiver ->
+    val recipients = to.map { it.replace("+", "") }
+    val recipient = receiver.replaceFirst("+", "")
+    recipient in recipients
+}
 
 fun LocalSmsOutbox(capacity: Int = LocalOutbox.DEFAULT_CAPACITY) = LocalOutbox(capacity, isSmsSent)
